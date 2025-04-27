ff1b348f95a41798c6b46ab50a8e24ec88ef3ff3
Advait Sankhe <124705956+AdvaitSan@users.noreply.github.com>
2025-04-27 20:32:33 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -7,11 +7,10 @@ class Config:
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     
     # MongoDB configuration
-    MONGO_URI = os.environ.get('mongodb+srv://aadityamalani15:yHFOhT72LbpT052L@cluster0.cu7v1af.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0') or 'mongodb://localhost:27017/'
     MONGO_DB_NAME = os.environ.get('local') or 'file_scanner'
     
     # VirusTotal configuration
-    VT_API_KEY = os.environ.get('ed85f6612c26d917c173df1a8547df9df0d9f114eaf1b73d15cd4e59558bf4da') or 'ed85f6612c26d917c173df1a8547df9df0d9f114eaf1b73d15cd4e59558bf4da'
+    VT_API_KEY = os.environ.get
     
     # Other configurations
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB max upload size
